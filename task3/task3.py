fa5fed8938f42945750c4a82b8ed80f277974810
mashinator2000 <128846350+mashinator2000@users.noreply.github.com>
2025-07-10 00:04:05 +0300

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,7 +1,36 @@
 # Задание 3: report.json
 
 import json
-with open('fortest/tests.json', 'r') as file:
-    data = json.load(file)
-data = data["tests"]
-print(data[0]["title"])
+
+file1, file2, file3 = [el for el in input().split()]
+with open(file1, 'r') as file:
+    tests_structure = json.load(file)
+
+
+with open(file2, 'r') as file:
+    values_list = json.load(file)
+values_list = values_list["values"]
+
+
+def update_test_values(tests_structure, values_list):
+    node_map = {}
+
+    def traverse(nodes):
+        for node in nodes:
+            node_map[node['id']] = node
+
+            if 'values' in node:
+                traverse(node['values'])
+
+    traverse(tests_structure['tests'])
+
+    for item in values_list:
+        if int(item['id']) in node_map:
+            node_map[item['id']]['value'] = item['value']
+
+    return tests_structure
+
+report = update_test_values(tests_structure, values_list)
+
+with open(file3, 'w') as file:
+    json.dump(report, file)
